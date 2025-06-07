56a040a02aae9526ea2d7e138d2d72b03aa82e80
Rajeev <rajeevranjan9560807144@gmail.com>
2025-06-07 12:13:48 +0530

    Initial commit

--- Database/DBConnection.js
+++ Database/DBConnection.js
@@ -9,14 +9,16 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
 
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
+        const conn = await mongoose.connect(process.env.MONGO_URI,
+            { useNewUrlParser: true, useUnifiedTopology: true }
         
-             }
+             
         )
         console.log(`connection  to mongoose Database ${conn.connection.host}`)
         
     } catch (error) {
         console.log(`error in mongoose ${error}`)
+        process.exit(1);  // Exit if DB connection fails
         
     }
 
