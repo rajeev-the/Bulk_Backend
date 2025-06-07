77110cc7141aaf2a38d724894c3a00e9789e7f1e
Rajeev <rajeevranjan9560807144@gmail.com>
2025-06-07 11:19:16 +0530

    Initial commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
